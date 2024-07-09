d1a033688095b6b1404f03484093417e88c754d3
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2024-07-09 12:56:32 +0100

    changes in Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Tree myFavoriteMapleTree = new Tree (25, 50,
                 100,TreeType.MAPLE);
+
         Tree myFavoriteOakTree = new Tree (55, 200,
                 300,TreeType.OAK);
 
+        myFavoriteOakTree.CallTallTree();
+        myFavoriteMapleTree.CallTallTree();
+
+        System.out.println(Tree.TRUNK_COLOR);
+        Tree.callATree();
     }
 }
